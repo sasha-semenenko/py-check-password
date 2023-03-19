bca5cc88b9406e6914f530d253dd3dfb46010b60
Sasha Semenenko <semenenko233249@gmail.com>
2023-03-19 18:53:24 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,15 +3,17 @@ from app.main import check_password
 
 
 @pytest.mark.parametrize(
-    "string_password, meaning_value",
+    "string_password, message_result",
     [
-        ('Pass@word1', True),
-        ('qwerty', False),
-        ('Str@ng', False)
+        pytest.param("Pass@word123456789", False, id="password too long"),
+        pytest.param("Pas@1", False, id="too short password"),
+        pytest.param("pass@word1", False, id="has not upper case"),
+        pytest.param("Pass@word", False, id="without digits"),
+        pytest.param("Password1", False, id="without special symbol"),
     ]
 )
 def test_check_password(
-        string_password,
-        meaning_value
+        string_password: str,
+        message_result: bool
 ) -> None:
-    assert check_password(string_password) == meaning_value
+    assert check_password(string_password) == message_result
